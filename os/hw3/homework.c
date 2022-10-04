b8cfff73b45b25b7a3355b5f516e30e897c86c51
Matt Go-School <71407082+kks219hyunju@users.noreply.github.com>
2022-10-04 00:44:00 -0400

    Update homework.c

--- os/hw3/homework.c
+++ os/hw3/homework.c
@@ -63,11 +63,42 @@ int main(int argc, char *argv[]) //delcaring input arguments in main function
 	}
 	else if(hflag == 1 && rflag = 0)
 	{
-		//long lines of code
+		printf(" -- arg1 name of the file -- arg2 delay of each line     
+		       The following program is to take first argument from 
+		       the user and read each line of the file given by the
+		       user and print out the line in reverse. The second 
+		       argument is the delay between printing of each line. 
+		       By using sleep function, the program is able to delay 
+		       the printing of each line before outputing next reserved line.")
 	}
 	else if(rflag == 1 && hflag = 0)
 	{
-		//long lines of code
+		char const* filename = cvalue; // First argument for file, this is pointer
+		char* time = .9; // delay time
+		ssize_t read; 
+		char* line = NULL; //point of the line is NULL as there cannot be no line in the beginning
+		size_t len = 0; // size is not defined until the file is called
+		int tims =0; // integer converted delay arugment will be stored here
+
+		tims = atoi(time); // convert the string to integer for delay time
+		FILE *myfile = fopen(filename,"r"); // open the file by given by pointer argument
+		while ((read = getline(&line, &len, myfile)) != -1)
+		{
+			printf("%s",line); //print line as it reads
+
+			int p = strlen(line)-2; //remove empty lines between the reserved line, detect number of characters
+
+			for(p; p>=0; p--) //printing words in reverse happens here
+			{
+				// as the for loop counts down, it will print out each letter from the end of the line
+				printf("%c",line[p]); 
+			}
+
+			printf("\n");
+			sleep(tims); //sleep for given time by the user
+		}
+
+		fclose(myfile); //close file
 	}
 	else
 	{
