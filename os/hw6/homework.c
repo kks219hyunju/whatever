f9b3577fc950e1c1348d81534cb2e6de15d277b8
Matt Go-School <71407082+kks219hyunju@users.noreply.github.com>
2022-10-25 11:58:32 -0400

    hw6

---
+++ os/hw6/homework.c
@@ -0,0 +1,191 @@
+// 1c Gyungseok Go
+// Operating Systems
+// Mr. Ethan Gold
+// Homework #5
+/*
+-h is help document
+-r is to read the file in reverse like hw2
+-c is needed for the file document
+The following program is to take first argument from the user
+and read each line of the file given by the user and
+print out the line in reverse. The second argument is
+the delay between printing of each line. By using "sleep"
+function, the program is able to delay the printing of each line
+before outputing next reserved line.
+*/
+
+
+//Calling in all required libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <getopt.h>
+
+void *wastemem(int a) {
+
+  int *mallblock = NULL;
+  long mallocnum = 1024*1024*1024; // 1 computer-million * user request
+
+  printf("malloc'ing a pile of RAM....\n");
+  mallblock = calloc(mallocnum, sizeof(int)); // of this architecture's integer
+  if (mallblock <= 0) {
+    char failmsg[128];
+    snprintf(failmsg, sizeof(failmsg), "failed to allocate malllock: %s\n", strerror(errno));
+    fprintf(stderr, "%s", failmsg);
+    return NULL;
+  } else {
+    // init with 1's. forces page table allocation because "lazy" optimizations
+    memset(mallblock, 1, mallocnum);
+    printf("successfully allocated mallblock\n");
+  }
+  return mallblock; // pointer to this block must be saved by caller, else leaky
+} // end wastemem()
+
+
+
+int readback(char* fileCall)
+{
+	char const* filename = fileCall; // First argument for file, this is pointer
+	FILE *myfile = NULL;
+	ssize_t read; 
+	char* line = NULL; //point of the line is NULL as there cannot be no line in the beginning
+	size_t len = 0; // size is not defined until the file is called
+	int tims = 1; // integer converted delay arugment will be stored here
+
+	if(fileCall == "hello")
+	{
+		while ((read = getline(&line, &len, stdin)) != -1)
+		{
+			printf("%s",line); //print line as it reads
+
+			int p = strlen(line)-2; //remove empty lines between the reserved line, detect number of characters
+
+			for(; p>=0; p--) //printing words in reverse happens here
+			{
+				// as the for loop counts down, it will print out each letter from the end of the line
+				printf("%c",line[p]); 
+			}
+
+			printf("\n");
+			sleep(tims); //sleep for given time by the user
+		}
+
+		fclose(myfile); //close file
+		return 0;
+	}
+	else
+	{
+		FILE *myfile = fopen(filename,"r"); // open the file by given by pointer argument
+		while ((read = getline(&line, &len, myfile)) != -1)
+		{
+			printf("%s",line); //print line as it reads
+
+			int p = strlen(line)-2; //remove empty lines between the reserved line, detect number of characters
+
+			for(; p>=0; p--) //printing words in reverse happens here
+			{
+				// as the for loop counts down, it will print out each letter from the end of the line
+				printf("%c",line[p]); 
+			}
+
+			printf("\n");
+			sleep(tims); //sleep for given time by the user
+		}
+
+		fclose(myfile); //close file
+		return 0;
+	}
+}
+
+void forkDothings(char* theThing)
+{
+
+  if (fork() == 0)
+    {
+      readback(theThing);
+    }
+  else
+    {
+      
+    }
+}
+
+
+
+int main(int argc, char* *argv) //delcaring input arguments in main function
+{
+	int hflag = 0;
+	int rflag = 0;
+	char *cvalue = NULL;
+	int index;
+	int opt;
+	int mvalue;
+	int mflag;
+	char *mcvar = NULL;
+	
+	opterr = 0;
+
+	
+	while((opt= getopt(argc,argv,"rmh")) != -1)
+	{
+		switch(opt)
+		{
+			case 'm':
+				mflag = 1;
+				mcvar = optarg;
+				mvalue = atoi(mcvar);
+				break;
+			case 'r':
+				// check for r argument
+				rflag = 1;
+				break;
+			case 'h':
+				// check for h argument
+				hflag = 1;
+				break;
+			case ':':
+				printf("Missing arg for %c\n", optopt);
+				break;
+			case '?':
+				printf("Unknown option: %c\n", optopt);
+				break;
+		}
+		
+		if(hflag == 0 && rflag == 1 && optind < argc)
+		{
+		 do
+		   {
+			wastemem(mvalue);
+			forkDothings(argv[optind]);
+		   }
+		   while(++optind < argc);
+		}
+		else if(hflag == 1 && rflag == 0)
+		{
+		  printf("USAGE: .main.out/ -c name of the file -r delay of each line or cat filename.txt | ./main.out -r -c \n");
+		  // help functoin
+		}
+		else if(rflag == 1 && hflag == 1)
+	        {
+		  printf("PASS IN ONLY ONE VARIABLE\n");
+		}
+		else if(rflag == 1 && hflag == 0 && cvalue == NULL)
+		{
+		  readback("hello");
+		}
+		else
+		{
+		  printf("FAILED!\n");	//if nothing is called, its failed
+		}
+	}
+	for (index = optind; index < argc; index++)
+    		printf ("Non-option argument %s\n", argv[index]); //if no argument is selected.
+
+	while(wait(NULL)>0);
+	return 0;
+	
+ }
