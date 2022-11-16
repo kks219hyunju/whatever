aae601f6295e86b8375cc9da75e310c42b9f0bd0
Matt Go-School <71407082+kks219hyunju@users.noreply.github.com>
2022-11-15 20:35:37 -0500

    hotfix

---
+++ os/hw7/tinycgi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## Minimal cgi demo wrapper to show how the CGI environment works.
+## Header should be in child program, but this is a simple demo
+
+## dump the env as if it were a shell.
+## Human: Look for URL parameters in the outp
+
+./a.out -rL /var/log/osLogfile.log -H file.txt;
+
+env
